02c63ea984d5d0ee898c25aaf3f27e588dbda8b9
drumpt <ssohot1@kaist.ac.kr>
2021-01-17 01:08:27 +0900

    Finish everything except build

---
+++ Assets/Scripts/CameraAuthorizer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_ANDROID
+    using UnityEngine.Android;
+#elif UNITY_IOS
+    using UnityEngine.iOS;
+#endif
+
+public class CameraAuthorizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        #if PLATFORM_ANDROID
+        if(!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+        #elif PLATFORM_IOS
+        if(!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+        #endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PhoneCamera.cs
+++ Assets/Scripts/PhoneCamera.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System;
+using System.IO;
 using System.Text;
 using System.Collections.Generic;
 using OpenCVForUnity;
@@ -22,10 +23,16 @@ public class PhoneCamera : MonoBehaviour
 
     Mat frame;
     CascadeClassifier faceDetector;
-    private string cascadeClassifierPath = @"Assets/Resources/haarcascade_frontalface_default.xml";
-    private string modelSourcePath = "facial_expression_model_sad_neutral_enhenced"; // best model so far
-    // private string modelSourcePath = "facial_expression_model_lightweighted"; // best model so far
+    private string cascadeClassifierPath = "haarcascade_frontalface_default.xml";
+    // private string cascadeClassifierPath = @"Assets/StreamingAssets/haarcascade_frontalface_default.xml";
+    // private string cascadeClassifierPath = @"Assets/Resources/haarcascade_frontalface_default.xml";
+    // private string cascadeClassifierPath = "haarcascade_frontalface_default";
+    private string modelSourcePath = "facial_expression_model_sad_neutral_enhenced";
+    // private string modelSourcePath = @"Assets/StreamingAssets/facial_expression_model_sad_neutral_enhenced"; // best model so far
+    // private string modelSourcePath = "facial_expression_model_sad_neutral_enhenced"; // best model so far
+    // private string modelSourcePath = "facial_expression_model_lightweighted";
     // private string modelSourcePath = "facial_expression_model_sad_enhenced";
+
     private int inputSize = 48;
 
     Dictionary<int, string> indexToEmotions = new Dictionary<int, string>() {{0, "angry"},  {1, "happy"}, {2, "neutral"}, {3, "sad"}, {4, "surprise"}};
@@ -38,6 +45,38 @@ public class PhoneCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(Application.platform);
+        Debug.Log(Application.streamingAssetsPath);
+        if(Application.platform == RuntimePlatform.OSXEditor)
+        {
+            cascadeClassifierPath = Application.dataPath + "/StreamingAssets/" + cascadeClassifierPath;
+        }
+        else if(Application.platform == RuntimePlatform.Android)
+        {
+            cascadeClassifierPath = "jar:file://" + Application.dataPath + "!/assets/" + cascadeClassifierPath;
+            byte[] FileBytes;
+            if(cascadeClassifierPath.Contains("://"))
+            {
+                WWW www = new WWW(cascadeClassifierPath);
+                while(!www.isDone) {}
+                FileBytes = www.bytes;
+            }
+            else
+            {
+                FileBytes = File.ReadAllBytes(cascadeClassifierPath);
+            }
+            cascadeClassifierPath = string.Format("{0}/{1}", Application.persistentDataPath, "haarcascade_frontalface_default.xml");
+            File.WriteAllBytes(cascadeClassifierPath, FileBytes);
+        }
+        else if(Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            cascadeClassifierPath = "file:" + Application.dataPath + "/Raw/" + cascadeClassifierPath;
+        }
+        else
+        {
+            cascadeClassifierPath = Application.dataPath + "/StreamingAssets/" + cascadeClassifierPath;
+        }
+
         defaultBackground = background.texture;
         WebCamDevice[] devices = WebCamTexture.devices;
         
@@ -76,24 +115,25 @@ public class PhoneCamera : MonoBehaviour
         now = DateTimeOffset.UtcNow;
         previousTime = currentTime;
         currentTime = now.ToUnixTimeMilliseconds();
-        Debug.Log("Start 1");
-        Debug.Log(currentTime - previousTime);
+        // Debug.Log("Start 1");
+        // Debug.Log(currentTime - previousTime);
 
+        // model = ModelLoader.Load((NNModel)modelSourcePath);
         model = ModelLoader.Load((NNModel)Resources.Load(modelSourcePath));
 
         now = DateTimeOffset.UtcNow;
         previousTime = currentTime;
         currentTime = now.ToUnixTimeMilliseconds();
-        Debug.Log("Start 2");
-        Debug.Log(currentTime - previousTime);
+        // Debug.Log("Start 2");
+        // Debug.Log(currentTime - previousTime);
 
         worker = WorkerFactory.CreateWorker(WorkerFactory.Type.CSharpBurst, model); // synchronized execution with CPU usage
 
         now = DateTimeOffset.UtcNow;
         previousTime = currentTime;
         currentTime = now.ToUnixTimeMilliseconds();
-        Debug.Log("Start 3");
-        Debug.Log(currentTime - previousTime);
+        // Debug.Log("Start 3");
+        // Debug.Log(currentTime - previousTime);
     }
 
     // Update is called once per frame
@@ -105,7 +145,7 @@ public class PhoneCamera : MonoBehaviour
         DateTimeOffset now = DateTimeOffset.UtcNow;
         long previousTime = 0;
         long currentTime = now.ToUnixTimeMilliseconds();
-        Debug.Log("Update 1");
+        // Debug.Log("Update 1");
         // Debug.Log(currentTime - previousTime);
 
         float ratio = (float)frontCam.width / (float)frontCam.height;
@@ -119,8 +159,8 @@ public class PhoneCamera : MonoBehaviour
         now = DateTimeOffset.UtcNow;
         previousTime = currentTime;
         currentTime = now.ToUnixTimeMilliseconds();
-        Debug.Log("Update 2");
-        Debug.Log(currentTime - previousTime);
+        // Debug.Log("Update 2");
+        // Debug.Log(currentTime - previousTime);
 
         frame = new Mat(frontCam.height, frontCam.width, CvType.CV_8UC4);
         Utils.webCamTextureToMat(frontCam, frame);
@@ -134,8 +174,8 @@ public class PhoneCamera : MonoBehaviour
         now = DateTimeOffset.UtcNow;
         previousTime = currentTime;
         currentTime = now.ToUnixTimeMilliseconds();
-        Debug.Log("Update 3");
-        Debug.Log(currentTime - previousTime);
+        // Debug.Log("Update 3");
+        // Debug.Log(currentTime - previousTime);
 
         OpenCVForUnity.CoreModule.Rect[] faces = matOfRectFaces.toArray();
 
@@ -161,8 +201,8 @@ public class PhoneCamera : MonoBehaviour
             now = DateTimeOffset.UtcNow;
             previousTime = currentTime;
             currentTime = now.ToUnixTimeMilliseconds();
-            Debug.Log("Update 4");
-            Debug.Log(currentTime - previousTime);
+            // Debug.Log("Update 4");
+            // Debug.Log(currentTime - previousTime);
 
             Tensor tensor = new Tensor(resizedGrayFaceTexture);
             if (worker == null) return;
@@ -170,27 +210,27 @@ public class PhoneCamera : MonoBehaviour
             now = DateTimeOffset.UtcNow;
             previousTime = currentTime;
             currentTime = now.ToUnixTimeMilliseconds();
-            Debug.Log("Update 5-1");
-            Debug.Log(currentTime - previousTime);
+            // Debug.Log("Update 5-1");
+            // Debug.Log(currentTime - previousTime);
 
             output = worker.Execute(tensor).PeekOutput();
 
             now = DateTimeOffset.UtcNow;
             previousTime = currentTime;
             currentTime = now.ToUnixTimeMilliseconds();
-            Debug.Log("Update 5-2");
-            Debug.Log(currentTime - previousTime);
+            // Debug.Log("Update 5-2");
+            // Debug.Log(currentTime - previousTime);
 
             int index = output.ArgMax()[0];
 
             now = DateTimeOffset.UtcNow;
             previousTime = currentTime;
             currentTime = now.ToUnixTimeMilliseconds();
-            Debug.Log("Update 5-3");
-            Debug.Log(currentTime - previousTime);
+            // Debug.Log("Update 5-3");
+            // Debug.Log(currentTime - previousTime);
 
             emotion = indexToEmotions[index];
-            // Debug.Log(emotion);
+            Debug.Log(emotion);
             switch(emotion)
             {
                 case "happy":
@@ -224,8 +264,8 @@ public class PhoneCamera : MonoBehaviour
             now = DateTimeOffset.UtcNow;
             previousTime = currentTime;
             currentTime = now.ToUnixTimeMilliseconds();
-            Debug.Log("Update 5");
-            Debug.Log(currentTime - previousTime);
+            // Debug.Log("Update 5");
+            // Debug.Log(currentTime - previousTime);
         }
     }
 
